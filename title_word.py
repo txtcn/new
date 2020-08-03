028a727d84a8efc11cb73f51380739844d3bcfb3
张沈鹏 <zsp.042@gmail.com>
2020-08-03 23:46:59 +0800

    master

--- title_word.py
+++ title_word.py
@@ -10,11 +10,6 @@ from itertools import groupby
 utf8 = 'utf8'
 
 
-def longest_match(ac, txt):
-  for pos, match_set in groupby(ac.finditer(txt), itemgetter(1)):
-    yield max(match_set)
-
-
 def ngram(li, size):
   for n in range(2, size):
     for i in ngrams(li, n):
@@ -23,43 +18,31 @@ def ngram(li, size):
 
 def ngram_line(txt):
   for s in RE_PUNCTUATION.split(txt):
-    li = tuple(i.encode(utf8) for i in tokenize(s))
+    li = tokenize(s)
     for i in ngram(li, 12):
       yield i
 
 
 def parse_word(title, txt):
-  count = Counter()
+  count = {}
 
-  word_set = set()
-  _title = title.decode(utf8, 'ignore')
-  if _title.encode(utf8) != title:
-    print(title)
-    print(_title.encode(utf8))
-    print(_title)
-  title = _title
+  # word_set = set()
+  total = len(title) + len(txt)
+
   for i in ngram_line(title):
     if len(i) > 1:
-      i = b"".join(i)
+      count[i] = 1
+
+  for i in ngram_line(txt):
+    if i in count:
       count[i] += 1
-      word_set.add(i)
 
-  builder = AcoraBuilder(*tuple(word_set))
-  ac = builder.build()
-
-  for word, pos in longest_match(ac, txt):
-    count[word] += 1
-  total = len(title) + len(txt)
-  # for line in txt:
-  #   for i in ngram_line(line):
-  #     if i in count:
-  #       count[i] += 1
-  #
-  # for word, n in sorted(count.items(), key=lambda x: len(x[0])):
-  #   if n > 3:
-  #     if len(word) > 2:
-  #       for i in ngram(word, len(word)):
-  #         count[i] -= n
+  for word, n in sorted(count.items(), key=lambda x: len(x[0])):
+    if n >= 3:
+      if len(word) > 2:
+        for i in ngram(word, len(word)):
+          if i in count:
+            count[i] -= n
 
   r = []
   for word, n in count.items():
@@ -68,34 +51,32 @@ def parse_word(title, txt):
   return r
 
 
-ARROW = "➜".encode(utf8)
+ARROW = "➜"
 
 
 def _parse(filepath):
   count = Counter()
 
   def _word(title, txt):
-    for i in parse_word(title, b"\n".join(txt)):
+    for i in parse_word(title, "\n".join(txt[1:])):
       count[i] += 1
 
   total = 0
 
-  with zd.open(filepath, "rb") as f:
+  with zd.open(filepath) as f:
     it = iter(f)
     title = None
     txt = []
     for i in it:
       i = i.lower()
       if i.startswith(ARROW):
-        next(it)
         if title:
           _word(title, txt)
-        title = i[3:]
+        title = i[1:]
         txt = []
         total += 1
       else:
-        if i:
-          txt.append(i)
+        txt.append(i)
     if title:
       _word(title, txt)
 
@@ -121,29 +102,28 @@ if __name__ == "__main__":
   outfile = abspath(__file__)[:-2] + "txt"
   print(outfile)
   parse = Parse()
-  # with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
-  todo = {}
-  for root, _, file_li in walk("/share/txt/data"):
-    for filename in file_li:
-      if filename.endswith(".zd"):
-        filepath = join(root, filename)
-        _parse(filepath)
-        # todo[executor.submit(_parse, filepath)] = filepath
-  for future in as_completed(todo):
-    filepath = todo[future]
-    print(">", filepath)
-    try:
-      r = future.result()
-    except Exception as exc:
-      print(exc)
-    else:
-      parse(*r)
-      min_n = max(int(parse.total * 0.00001), 3)
-      t = [b"%s" % parse.total]
-      for k, v in sorted(parse.count.items(), key=itemgetter(1), reverse=True):
-        if v > min_n:
-          t.append(b"%s,%s" % (k, v))
-      t = b"\n".join(t)
-      print(t)
-      with open(outfile, "wb") as out:
-        out.write(t)
+  with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
+    todo = {}
+    for root, _, file_li in walk("/share/txt/data"):
+      for filename in file_li:
+        if filename.endswith(".zd"):
+          filepath = join(root, filename)
+          # _parse(filepath)
+          todo[executor.submit(_parse, filepath)] = filepath
+    for future in as_completed(todo):
+      filepath = todo[future]
+      print(">", filepath)
+      try:
+        r = future.result()
+      except Exception as exc:
+        print("Exception", exc)
+      else:
+        parse(*r)
+        min_n = max(int(parse.total * 0.00001), 3)
+        t = [str(parse.total)]
+        for k, v in sorted(parse.count.items(), key=itemgetter(1), reverse=True):
+          if v > min_n:
+            t.append("%s,%s" % ("".join(k), v))
+        t = "\n".join(t)
+        with open(outfile, "w") as out:
+          out.write(t)
