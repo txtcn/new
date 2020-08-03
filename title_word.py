cbe960cf9b1f824bb253a5a7a76ea09c431888fc
张沈鹏 <zsp.042@gmail.com>
2020-08-03 19:50:31 +0800

    master

--- title_word.py
+++ title_word.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
         parse(*r)
         min_n = max(int(parse.total * 0.00001), 3)
         with open(outfile, "wb") as out:
-          out.write(str(parse.total) + "\n")
+          out.write(bytes(parse.total) + b"\n")
           for k, v in sorted(parse.count.items(), key=itemgetter(1), reverse=True):
             if v > min_n:
               out.write(b"%s,%s\n" % (k, v))
