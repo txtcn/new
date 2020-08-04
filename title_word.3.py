27abea2aef1a5e9aa90b66acef2fa8fa700ce017
张沈鹏 <zsp.042@gmail.com>
2020-08-04 16:44:28 +0800

    master

--- title_word.3.py
+++ title_word.3.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
-# from time import time
+from _title_word import total_count
+from operator import itemgetter
 
-# def main():
-#   pass
+
+def main():
+  total_title, count_title = total_count("title_word")
+  total_txt, count_txt = total_count("title_word.2")
+  total_txt += 1
+  r = []
+  for k, v in total_title.items():
+    p = (v * total_txt) / ((count_txt.get(k, 0) + 1) * total_title)
+    r.append((k, p))
+  for i in sorted(r, key=itemgetter(1)):
+    print("%s,%s" % i)
+
 
 if __name__ == "__main__":
-  import single_process.init
-  # main()
-  pass
-
+  main()
